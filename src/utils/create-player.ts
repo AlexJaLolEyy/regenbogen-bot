fdef0db54330f6acecc83f024c5861cd2b1f1c6d
Alex <alexanderbenski@gmail.com>
2025-05-04 03:37:48 +0200

    🔥  remove custom types and player creation utility to streamline codebase

--- src/utils/create-player.ts
+++
@@ -1,9 +0,0 @@
-import { Player } from 'discord-player';
-import { DefaultExtractors } from '@discord-player/extractor';
-import type { Client } from 'discord.js';
-
-export const createPlayer = async (client: Client) => {
-  const player = new Player(client);
-  await player.extractors.load(DefaultExtractors);
-  return player;
-};
